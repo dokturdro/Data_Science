07a50e3bcf5c7d6d9182a9874e67acd23150e2a9
dokturdro <35141154+dokturdro@users.noreply.github.com>
2018-05-24 21:49:19 +0200

    Add files via upload

--- linear_reg_pract.py
+++ linear_reg_pract.py
@@ -1,6 +1,9 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from scipy import sparse
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 quandl.ApiConfig.api_key = "EzAELjGNysBk6Nmf5Zn4"
 
@@ -15,7 +18,17 @@ forecast_col = 'Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.1*len(df)))
-
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-print(df.head())
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+acc = clf.score(X_test, y_test)
+
+print(acc)
