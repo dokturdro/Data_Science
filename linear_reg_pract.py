b3a88549bcaa0cec5412c8fb7e18498c7eec08ad
dokturdro <35141154+dokturdro@users.noreply.github.com>
2018-05-28 23:29:19 +0200

    Add files via upload

--- linear_reg_pract.py
+++ linear_reg_pract.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import quandl, math
 import numpy as np
+import datetime
 from scipy import sparse
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 quandl.ApiConfig.api_key = "EzAELjGNysBk6Nmf5Zn4"
 
@@ -28,8 +34,8 @@ y = np.array(df)
 
 X_lately = X[-forecast_out:]
 
-##df.dropna(inplace=True)
-##y = np.array(df['label'])
+df.dropna(inplace=True)
+y = np.array(df['label'])
 
 print(len(X), len(y))
 
@@ -42,3 +48,22 @@ acc = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
 
 print(forecast_set, acc, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] =[np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
