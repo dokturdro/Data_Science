50c7d4d2e4d8847327ed2c950c427852b12a7c9e
dokturdro <35141154+dokturdro@users.noreply.github.com>
2019-03-14 19:33:24 +0100

    Add files via upload

---
+++ eda_mall.py
@@ -0,0 +1,19 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+df = pd.read_csv(r"C:\Users\Administrator\Desktop\datasets\business\Mall_Customers.csv")
+print(df.columns.values)
+print(df.head())
+
+#sns.countplot(df["Gender"].dropna())
+features = ['Age', 'Annual Income (k$)', 'Spending Score (1-100)']
+#df[features].hist(figsize=(10, 6))
+df_corr = df.corr()
+#sns.heatmap(df_corr, cbar=False, annot=True)
+
+# sns.jointplot(x='Annual Income (k$)', y='Spending Score (1-100)', data=df, kind='scatter')
+sns.boxplot(data=df.ix[:,1:3])
+
+plt.show()
