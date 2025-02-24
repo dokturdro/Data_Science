6a38a515c6f0fbdccd5ce4c5d33c54b4ae47e260
dokturdro <m.dro@vivaldi.net>
2025-02-24 17:39:41 +0100

    Delete old files

--- clf_flights/plots.py
+++
@@ -1,7 +0,0 @@
-def plot_heatmap(data):
-    matrix = np.triu(data.corr())
-    plt.figure(figsize=(8, 8))
-    heatmap = sns.heatmap(data.corr(), annot=True, annot_kws={"fontsize": 8}, mask=matrix, linewidths=0.2, square=True)
-    figure = heatmap.get_figure()
-
-    return figure
